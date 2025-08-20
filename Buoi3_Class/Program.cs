129b3d42f06035b5718874e5a773b4e979966f81
HEJIN2006 <nguyenthithulan12121982@gmail.com>
2025-08-20 18:18:49 +0700

    Add files via upload

---
+++ Buoi3_Class/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi3_Class
+{
+
+    // Tên hàng                  Gạo Ngô Khoa
+    // Số lượng(tấn)              100	90	20
+    // Đơn giá(nghìn đồng/1kg)    30	15	12
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.OutputEncoding = Encoding.UTF8;
+            //SanPham sp1 = new SanPham();
+            //sp1.XuatSanPham();
+            //Console.WriteLine($"tên sản phẩm: {sp1.TenSanPham}");
+            //Console.WriteLine($"số lượng: {sp1.SoLuong}");
+            //Console.WriteLine($"đơn giá: {sp1.DonGia}");
+            //sp1.TenSanPham = "Đường";
+            //sp1.SoLuong = 20;
+            //sp1.DonGia = 30000;
+            //sp1.XuatSanPham();
+            //Console.WriteLine($"Tên sản phẩm: {sp1.TenSanPham}");
+            //Console.WriteLine($"Số lượng: {sp1.SoLuong}");
+            //Console.WriteLine($"Đơn giá: {sp1.DonGia}");
+            //SanPham sp2 = new SanPham("Mía", 25, 10000);
+            //sp2.XuatSanPham();
+            //Console.WriteLine($"Tên sản phẩm: {sp2.TenSanPham}");
+            //Console.WriteLine($"Số lượng: {sp2.SoLuong}");
+            //Console.WriteLine($"Đơn giá: {sp2.DonGia}");
+            //SanPham sp3 = new SanPham();
+            //sp3.NhapSanPham()
+            //sp3.XuatSanPham();
+            //SanPham.NhapSanPham().XuatSanPham();
+            //SanPham sp3 = SanPham.NhapSanPham();
+            //sp3.XuatSanPham();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int n = 3;
+            SanPham[] l = new SanPham[n];
+            SanPham.Nhapdulieulieu(l,n);
+            SanPham.Xuatdulieulieu(l,n);
+        }
+    }
+}
---
+++ Buoi3_Class/SanPham.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi3_Class
+{
+    internal class SanPham
+    {
+        private string tensp;
+        private int soluong;
+        private double dongia;
+
+        public SanPham()
+        {
+            this.tensp = "No name";
+            this.soluong = 0;
+            this.dongia = 0;
+        }
+        public SanPham(string tensp, int sl, double g)
+        {
+            this.tensp = tensp;
+            this.soluong = sl;
+            this.dongia = g;
+        }
+
+        public string TenSanPham
+        {
+            get { return tensp; }
+            set { tensp = value; }
+        }
+
+        public int SoLuong
+        {
+            get { return soluong; }
+            set { soluong = value; }
+        }
+
+        public double DonGia
+        {
+            get { return dongia; }
+            set { dongia = value; }
+        }
+        public static SanPham NhapSanPham()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            SanPham p = new SanPham();
+            Console.WriteLine("Nhập tên sản phẩm: ");
+            p.tensp = Console.ReadLine();
+            Console.WriteLine("Nhập số Lượng: ");
+            p.soluong = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập đơn giá: ");
+            p.dongia = double.Parse(Console.ReadLine());
+            return p;
+        }
+        public void  XuatSanPham()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine($"Tên sản phẩm: {tensp}");
+            Console.WriteLine($"Số lượng: {soluong}");
+            Console.WriteLine($"Đơn giá: {dongia}");
+        }
+        public static void Nhapdulieulieu(SanPham[] a, int n)
+        {
+            SanPham[] ds = new SanPham[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Nhập thông tin cho phàn tử thứ:  {i + 1}");
+                ds[i] = NhapSanPham();
+            }
+        }
+        public static void Xuatdulieulieu(SanPham[] listsp, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"San pham thu {i + 1}");
+                listsp[i].XuatSanPham();
+            }
+        }
+    }
+}
